ada82ecfdac29d35e7b2c547b1f37768f8ea8d56
Joanna Wrzesinska <joanna.n.wrzesinska@gsk.com>
2021-10-27 14:44:29 +0200

    feat(Navbar): added navbar component plus js  css

---
+++ react-website-form/src/components/Button.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Button.css";
+
+const STYLES = ["btn-primary", "btn--disabled"];
+const COLOR = ["primary", "blue", "red"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  buttonColor,
+}) => {
+  const checkButtonStyles = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLE[0];
+
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : COLOR[0];
+  return (
+    <button
+      className={`btn ${checkButtonStyles} ${checkButtonColor}`}
+      type={type}
+      onClick={onClick}>
+      {children}
+    </button>
+  );
+};
---
+++ react-website-form/src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdFilterHdr } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            <MdFilterHdr className='navbar-icon' />
+            LOGO
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className='nav-item'>
+              <Link to='/form' className='nav-links' onClick={closeMobileMenu}>
+                Form
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/profile'
+                className='nav-links'
+                onClick={closeMobileMenu}>
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
